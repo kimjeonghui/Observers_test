5360cf90246d3182a962d331a7716160d76cf1f7
SuGyoungIn <isks2838@gmail.com>
2024-02-02 18:20:23 +0900

    [FE]feat:prettier 문법에 안맞는거 수정(개행 문제)

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -48,9 +48,10 @@ registerRoute(
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
+// Add in any other file extensions or routing criteria as needed.
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
