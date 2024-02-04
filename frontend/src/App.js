436681c1b2072dd203a5a5d4d3b14ec0c46f5551
SuGyoungIn <isks2838@gmail.com>
2024-02-04 22:49:45 +0900

    [FE]feat: Home 컴포넌트 안에서 router 작업을 위해 Home의 주소값 변경. Home 안에는 Invoice route추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/*' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 import { Bg } from '../components/global/GlobalStyles';
+import CustomContainer from '../components/global/Container';
+import { Routes, Route } from 'react-router-dom';
+import Invoice from './Invoice';
+
 export default function Home(props) {
   return (
     <div>
-      <Bg>홈입니다.</Bg>
+      <Bg>
+        <CustomContainer width='80' height='78'>
+          <Routes>
+            <Route path='/' element={<h1>Home Page</h1>} />
+            <Route path='/invoice' element={<Invoice />} />
+          </Routes>
+        </CustomContainer>
+      </Bg>
     </div>
   );
 }
