ef356727fc9de7961911aa25b998a3ef85c2a2fa
SuGyoungIn <isks2838@gmail.com>
2024-02-05 10:55:01 +0900

    [FE]fix: styled-component 함수 밖으로 이동

--- frontend/src/components/global/Container.jsx
+++ frontend/src/components/global/Container.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import styled from 'styled-components';
+const ContainerDiv = styled.div`
+  width: ${(props) => props.width + 'vw'};
+  min-width: 320px;
+  height: ${(props) => props.height + 'vh'};
+  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.8);
+`;
 export default function Container(props) {
   const { width, height } = props;
-  const ContainerDiv = styled.div`
-    width: ${width}vw;
-    min-width: 320px;
-    height: ${height}vh;
-    border-radius: 15px;
-    background-color: rgba(255, 255, 255, 0.8);
-  `;
-  return <ContainerDiv>{props.children}</ContainerDiv>;
+
+  return (
+    <ContainerDiv width={width} height={height}>
+      {props.children}
+    </ContainerDiv>
+  );
 }
