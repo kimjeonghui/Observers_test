798f5316d7aa2119ab0310ccde63a23723619bef
csh <tmdgml1754@naver.com>
2024-02-05 10:39:47 +0900

    [BE] Feat: 사용자 정보 수정

--- backend/user-service/src/main/java/com/posco/userservice/service/UserService.java
+++ backend/user-service/src/main/java/com/posco/userservice/service/UserService.java
@@ -2,10 +2,12 @@ package com.posco.userservice.service;
 
 import com.posco.userservice.dto.request.LoginDTO;
 import com.posco.userservice.dto.request.RegisterDTO;
+import com.posco.userservice.dto.request.UpdateDTO;
 import com.posco.userservice.dto.response.TokenDTO;
 
 public interface UserService {
     TokenDTO registerUser(RegisterDTO registerDTO);
     TokenDTO loginUser(LoginDTO loginDTO);
     boolean checkExistName(String name);
+    TokenDTO updateUser(UpdateDTO updateDTO);
 }
