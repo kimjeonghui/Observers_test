9ddca02f15059c4f5e73b816efa4d9614d00261e
kimjoenghui <20190896@sungshin.ac.kr>
2024-02-06 10:17:23 +0900

    [FE] feat: 회계 전표, 결재 승인 페이지 작성

--- frontend/src/pages/AccountingSlip.jsx
+++ frontend/src/pages/AccountingSlip.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import AccountingSlipTable from '../components/accoutingSlip/AccountingSlipTable';
+import CollapsibleTable from '../components/accoutingSlip/CollapsibleTable';
+import {
+  SpanTab,
+  ActiveSpanTab,
+} from '../components/accoutingSlip/AccountingSlipStyles';
 
 export default function AccountingSlip(props) {
+  const [activeTab, setActiveTab] = useState(0);
+  const tabMenus = ['결재 승인', '회계전표'];
+
+  const handleTabChange = (idx) => {
+    setActiveTab(idx);
+  };
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
   // dateRange는 [startDate, endDate] 형태의 배열을 값 가짐
@@ -13,30 +25,30 @@ export default function AccountingSlip(props) {
   return (
     <div>
       <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '300px',
-          margin: '0 auto',
-        }}
+      // style={{
+      //   display: 'flex',
+      //   flexDirection: 'row',
+      //   justifyContent: 'space-between',
+      //   alignItems: 'center',
+      //   width: '300px',
+      //   margin: '0 auto',
+      // }
+      // }
       >
-        <button>앞</button>
-        <div>{currentMonth}</div>
-        <button>뒤</button>
+        {tabMenus.map((tab, idx) =>
+          idx === activeTab ? (
+            <ActiveSpanTab key={idx} onClick={() => handleTabChange(idx)}>
+              {tab}
+            </ActiveSpanTab>
+          ) : (
+            <SpanTab key={idx} onClick={() => handleTabChange(idx)}>
+              {tab}
+            </SpanTab>
+          )
+        )}
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <DatePicker
-          selectsRange={true}
-          startDate={startDate}
-          endDate={endDate}
-          onChange={(update) => {
-            setDateRange(update);
-          }}
-          withPortal
-        />
-      </div>
+      {activeTab === 0 && <CollapsibleTable />}
+      {activeTab === 1 && <AccountingSlipTable />}
     </div>
   );
 }
