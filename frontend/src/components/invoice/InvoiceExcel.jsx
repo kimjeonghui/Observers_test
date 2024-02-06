a72e4053419dda43296b13385417e5545afd6d38
SuGyoungIn <isks2838@gmail.com>
2024-02-06 13:10:07 +0900

    [FE] feat: 엑셀 색깔 커스텀 기능 추가중

--- frontend/src/components/invoice/InvoiceExcel.jsx
+++ frontend/src/components/invoice/InvoiceExcel.jsx
@@ -1,6 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import Spreadsheet from 'react-spreadsheet';
 import Button from '../global/Button';
+import classnames from 'classnames';
+
+// // cell 클릭시 배경색깔 바뀌게 하는 코드
+
+// const CustomCell = ({
+//   column,
+//   row,
+//   setCellDimensions,
+//   active,
+//   data,
+//   evaluatedData,
+//   DataViewer,
+//   setCellData,
+// }) => {
+//   // useRef를 사용하여 rootRef를 생성
+
+//   const HEIGHT = '1em';
+//   const WIDTH = '6em';
+//   useEffect(() => {
+//     setCellDimensions(
+//       { row, column },
+//       {
+//         height: HEIGHT,
+//         width: WIDTH,
+//         left: WIDTH * (column + 1),
+//         top: HEIGHT * (row + 1),
+//       }
+//     );
+//   }, [setCellDimensions, column, row]);
+
+//   if (data && data.DataViewer) {
+//     ({ DataViewer, ...data } = data);
+//   }
+
+//   if (!data) {
+//     // data가 없을 경우 빈 td 엘리먼트를 반환
+//     return <td />;
+//   }
+
+//   const initialBackgroundColor = 'yellow';
+
+//   return (
+//     <td
+//       className={classnames(
+//         'Spreadsheet__cell',
+//         data && data.readOnly && 'Spreadsheet__cell--readonly',
+//         data && data.className
+//       )}
+//       style={{
+//         backgroundColor: active
+//           ? 'red'
+//           : data.row === 0 || data.column === 0
+//             ? 'blue'
+//             : initialBackgroundColor,
+//       }}
+//       tabIndex={0}
+//     >
+//       <DataViewer
+//         row={row}
+//         column={column}
+//         cell={data}
+//         evaluatedCell={evaluatedData}
+//         setCellData={setCellData}
+//       />
+//     </td>
+//   );
+// };
+
 export default function InvoiceExcel(props) {
   const [data, setData] = useState([
     [
@@ -27,6 +95,21 @@ export default function InvoiceExcel(props) {
     [],
     [],
   ]);
+
+  const initialBackgroundColor = 'yellow';
+
+  const initialData = data.map((row, rowIndex) =>
+    row.map((cell, columnIndex) => ({
+      ...cell,
+      value: cell.value || '',
+      style: {
+        backgroundColor:
+          rowIndex === 0 || columnIndex === 0 ? 'blue' : initialBackgroundColor,
+      },
+      row: rowIndex,
+      column: columnIndex,
+    }))
+  );
 
   const addColumn = () => {
     let tmpData = [...data];
@@ -57,7 +140,7 @@ export default function InvoiceExcel(props) {
         행 추가
       </Button>
       <div style={{ width: '100%', height: '100%', overflow: 'auto' }}>
-        <Spreadsheet data={data} onChange={setData} />
+        <Spreadsheet data={initialData} onChange={setData} />
       </div>
     </div>
   );
