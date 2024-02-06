22bc89ac5784b0b70fae2895970e98cac8c1869f
SoeunShin <21900395@handong.edu>
2024-02-06 11:20:46 +0900

    [FE] Feat: ReceiptsDetail 테이블 수정

--- frontend/src/pages/ReceiptsDetail.jsx
+++ frontend/src/pages/ReceiptsDetail.jsx
@@ -8,37 +8,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const TAX_RATE = 0.07;
-
-function ccyFormat(num) {
-  return `${num.toFixed(2)}`;
-}
-
-function priceRow(qty, unit) {
-  return qty * unit;
-}
-
-function createRow(desc, qty, unit) {
-  const price = priceRow(qty, unit);
-  return { desc, qty, unit, price };
-}
-
-function subtotal(items) {
-  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
-}
-
-const rows = [
-  createRow('MerchantAddress', 'C/SANTA MARIA DEL BLANCA, 32 41004 SEVILLA'),
-  createRow('MerchantName', 'YIFAN HUANG S.L.'),
-  createRow('MerchantPhoneNumber', '+34954220988'),
-  createRow('Total', 33.8),
-  createRow('TransactionDate', '2019-01-12'),
-];
-
-const invoiceSubtotal = subtotal(rows);
-const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-const invoiceTotal = invoiceTaxes + invoiceSubtotal;
-
 function ReceiptsMonth(props) {
   return (
     <Box
@@ -57,7 +26,6 @@ function ReceiptsMonth(props) {
           display: 'flex',
           flexDirection: 'row',
           gap: '20px',
-          // padding: '20px',
         }}
       >
         <img
@@ -65,32 +33,127 @@ function ReceiptsMonth(props) {
           alt='img'
           style={{ width: '500px', height: '600px', margin: '7px' }}
         />
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label='spanning table'>
+        <TableContainer sx={{ maxHeight: 720 }} component={Paper}>
+          <Table aria-label='spanning table'>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.desc}>
-                  <TableCell>{row.desc}</TableCell>
-                  <TableCell align='right'>{row.qty}</TableCell>
-                  <TableCell align='right'>{row.unit}</TableCell>
-                  <TableCell align='right'>{ccyFormat(row.price)}</TableCell>
-                </TableRow>
-              ))}
               <TableRow>
-                <TableCell rowSpan={3} />
-                <TableCell colSpan={2}>Subtotal</TableCell>
-                <TableCell align='right'>
-                  {ccyFormat(invoiceSubtotal)}
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  MerchantAddress
+                </TableCell>
+                <TableCell align='right' colSpan={2}>
+                  C/SANTA MARIA DEL BLANCA, 32 41004 SEVILLA
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>Tax</TableCell>
-                <TableCell align='right'>{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-                <TableCell align='right'>{ccyFormat(invoiceTaxes)}</TableCell>
+                <TableCell rowSpan={5} />
+                <TableCell colSpan={1} style={{ fontWeight: 'bold' }}>
+                  HouseNumber
+                </TableCell>
+                <TableCell align='right'>32</TableCell>
               </TableRow>
               <TableRow>
-                <TableCell colSpan={2}>Total</TableCell>
-                <TableCell align='right'>{ccyFormat(invoiceTotal)}</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>Road</TableCell>
+                <TableCell align='right'>C/SANTA MARIA DEL BLANCA</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>PostalCode</TableCell>
+                <TableCell align='right'>41004</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>City</TableCell>
+                <TableCell align='right'>SEVILLA</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  StreetAddress
+                </TableCell>
+                <TableCell align='right'>32 C/SANTA MARIA DEL BLANCA</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  MerchantName
+                </TableCell>
+                <TableCell align='right' colSpan={2}>
+                  YIFAN HUANG S.L.
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  MerchantPhoneNumber
+                </TableCell>
+                <TableCell align='right' colSpan={2}>
+                  +34954220988
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>Total</TableCell>
+                <TableCell align='right' colSpan={2}>
+                  33.8
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  TransactionDate
+                </TableCell>
+                <TableCell align='right' colSpan={2}>
+                  2019-01-12
+                </TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>1 Content</TableCell>
+                <TableCell align='right' colSpan={2}>
+                  2 MENU DE LA CASA 15,00 30,00
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell rowSpan={4} />
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  Description
+                </TableCell>
+                <TableCell align='right'>MENU DE LA CASA</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>Price</TableCell>
+                <TableCell align='right'>15</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>Quantity</TableCell>
+                <TableCell align='right'>2</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>TotalPrice</TableCell>
+                <TableCell align='right'>30</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>2 Content</TableCell>
+                <TableCell align='right' colSpan={2}>
+                  1 CERVEZA JAPONESA 3,80 3,80
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell rowSpan={4} />
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  Description
+                </TableCell>
+                <TableCell align='right'>CERVEZA JAPONESA</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>Price</TableCell>
+                <TableCell align='right'>3.8</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>Quantity</TableCell>
+                <TableCell align='right'>1</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell style={{ fontWeight: 'bold' }}>TotalPrice</TableCell>
+                <TableCell align='right'>3.8</TableCell>
               </TableRow>
             </TableBody>
           </Table>
