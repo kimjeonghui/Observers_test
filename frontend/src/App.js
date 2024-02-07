1f6d19b1ecb122e88f0759475ac2cb35ccb8e7db
SuGyoungIn <isks2838@gmail.com>
2024-02-07 11:08:35 +0900

    [FE] Refactor: router home.jsx에서 app.js로 이동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,36 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Invoice from './pages/Invoice';
+import Receipts from './pages/Receipts';
+import ReceiptsMonth from './pages/ReceiptsMonth';
+import ReceiptsDetail from './pages/ReceiptsDetail';
+import Ocr from './pages/Ocr';
+import AccountingSlip from './pages/AccountingSlip';
+import AdminCode from './pages/AdminCode';
+import AdminOffice from './pages/AdminOffice';
+import Users from './pages/Users';
+import Summary from './pages/Summary';
+import Navbar from './components/global/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<Home />} />
+          <Route element={<Navbar />} />
+          <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/invoice' element={<Invoice />} />
+          <Route path='/receipts' element={<Receipts />} />
+          <Route path='/receipts/:id' element={<ReceiptsMonth />} />
+          <Route path='/receipts/:id/:id' element={<ReceiptsDetail />} />
+          <Route path='/ocr' element={<Ocr />} />
+          <Route path='/accounting-slip' element={<AccountingSlip />} />
+          <Route path='/admin-code' element={<AdminCode />} />
+          <Route path='/admin-office' element={<AdminOffice />} />
+          <Route path='/users' element={<Users />} />
+          <Route path='/summary' element={<Summary />} />
         </Routes>
       </BrowserRouter>
     </div>
