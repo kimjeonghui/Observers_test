336181f9c5e8dd7966b862878ceb0d9e7b7fc9ff
SuGyoungIn <isks2838@gmail.com>
2024-02-07 11:10:16 +0900

    [FE] feat: 유저에 따라 페이지 이동기능 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home(props) {
+  const navigate = useNavigate();
+  // 로그인한 사람에 따라 href 해주기
+  useEffect(() => {
+    //if(관리자일때) navigate('/summary');
+    //else navigate('/invoice');
+  }, []);
+
   return <div>홈입니다.</div>;
 }
