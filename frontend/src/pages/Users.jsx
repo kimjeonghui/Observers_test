046f2f67bf25a38275b723b7303e3a4256c373dd
csh <tmdgml1754@naver.com>
2024-02-07 14:36:49 +0900

    [FE] Feat: 로그인 연결

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -14,6 +14,10 @@ import AdminHeader from './AdminHeader';
 import { createSvgIcon } from '@mui/material/utils';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
 
 export default function Users(props) {
   const [tableData, setTableData] = useState([]);
@@ -188,6 +192,11 @@ export default function Users(props) {
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -206,6 +215,20 @@ export default function Users(props) {
           <Input widthV='20' heightV='5' label='Search' />
         </Grid>
         <Grid item xs={6} textAlign='right'>
+          <FormControl>
+            <InputLabel id='demo-simple-select-label'>Age</InputLabel>
+            <Select
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={age}
+              label='Age'
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
           <Button size='sm' onClick={handleAddRow}>
             <PlusIcon color='white' />
           </Button>
