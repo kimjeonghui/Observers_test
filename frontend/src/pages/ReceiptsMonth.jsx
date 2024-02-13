4e97c110d1be18eb4b4609ca0849e797ff350f30
SoeunShin <21900395@handong.edu>
2024-02-13 17:48:21 +0900

    [FE] Feat: 영수증 업로드 및 OCR API 호출 구현

--- frontend/src/pages/ReceiptsMonth.jsx
+++ frontend/src/pages/ReceiptsMonth.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
 import receipt_img from '../assets/spain2.jpeg';
+import styled from 'styled-components';
+
+const ImgButton = styled.button`
+  margin: 4px;
+  background-color: #f6f6f6;
+  border: 2px solid #f6f6f6;
+  border-radius: 5px;
+  padding: 5px 10px 3px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ddd; /* 마우스 호버 시 배경색 변경 예제 */
+  }
+`;
 
 function ReceiptsMonth(props) {
   const navigate = useNavigate();
@@ -22,7 +36,20 @@ function ReceiptsMonth(props) {
       <Typography variant='h6' style={{ fontSize: '23px', marginLeft: '12px' }}>
         2023-10
       </Typography>
-      <button style={{ margin: '10px' }}>
+      <ImgButton>
+        <Typography variant='body1'>2023-10-03 14:23:11</Typography>
+        <img
+          src={receipt_img}
+          alt='img'
+          style={{
+            width: '150px',
+            height: '150px',
+            margin: '7px',
+          }}
+          onClick={() => handleButtonClick('1')}
+        />
+      </ImgButton>
+      <ImgButton>
         <Typography variant='body1'>2023-10-03 14:23:11</Typography>
         <img
           src={receipt_img}
@@ -30,8 +57,8 @@ function ReceiptsMonth(props) {
           style={{ width: '150px', height: '150px', margin: '7px' }}
           onClick={() => handleButtonClick('1')}
         />
-      </button>
-      <button style={{ margin: '10px' }}>
+      </ImgButton>
+      <ImgButton>
         <Typography variant='body1'>2023-10-03 14:23:11</Typography>
         <img
           src={receipt_img}
@@ -39,8 +66,8 @@ function ReceiptsMonth(props) {
           style={{ width: '150px', height: '150px', margin: '7px' }}
           onClick={() => handleButtonClick('1')}
         />
-      </button>
-      <button style={{ margin: '10px' }}>
+      </ImgButton>
+      <ImgButton>
         <Typography variant='body1'>2023-10-03 14:23:11</Typography>
         <img
           src={receipt_img}
@@ -48,8 +75,8 @@ function ReceiptsMonth(props) {
           style={{ width: '150px', height: '150px', margin: '7px' }}
           onClick={() => handleButtonClick('1')}
         />
-      </button>
-      <button style={{ margin: '10px' }}>
+      </ImgButton>
+      <ImgButton>
         <Typography variant='body1'>2023-10-03 14:23:11</Typography>
         <img
           src={receipt_img}
@@ -57,16 +84,7 @@ function ReceiptsMonth(props) {
           style={{ width: '150px', height: '150px', margin: '7px' }}
           onClick={() => handleButtonClick('1')}
         />
-      </button>
-      <button style={{ margin: '10px' }}>
-        <Typography variant='body1'>2023-10-03 14:23:11</Typography>
-        <img
-          src={receipt_img}
-          alt='img'
-          style={{ width: '150px', height: '150px', margin: '7px' }}
-          onClick={() => handleButtonClick('1')}
-        />
-      </button>
+      </ImgButton>
     </Box>
   );
 }
