4c1da83f225a8e35f94a9d3aee1eeb4d7d74abaa
SuGyoungIn <isks2838@gmail.com>
2024-02-13 16:53:01 +0900

    [FE] feat: navbar에서 sidebar로 합치고, link 걸어놓음 (반응형은 아직 안끝남)

--- frontend/src/components/global/Logo.jsx
+++ frontend/src/components/global/Logo.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 import LogoImg from '../../assets/posco_logo.png';
 
 export default function Logo(props) {
-  const { url, height } = props;
+  const { width, url, height } = props;
   const styles = {
+    width: width,
     height: height,
     background: `center / contain no-repeat url('${LogoImg}')`,
   };
