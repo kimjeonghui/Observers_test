65febcbe839bb6d5780ece981a565cf4d9be2b40
kimjoenghui <20190896@sungshin.ac.kr>
2024-02-15 15:43:10 +0900

    [FE] mics: 사용하지 않는 import 제거

--- frontend/src/pages/AccountingSlip.jsx
+++ frontend/src/pages/AccountingSlip.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import CheckboxAccountingSlipTable from '../components/accoutingSlip/CheckboxAccountingSlipTable';
 import AdminApprovalTable from '../components/accoutingSlip/ApprovalTable';
 import {
   SpanTab,
