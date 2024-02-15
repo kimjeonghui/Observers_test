4c0d61c781fbdc8e663fa8b441dc8f53b7005a19
SoeunShin <21900395@handong.edu>
2024-02-15 10:37:15 +0900

    [BE] Feat: refactoring

--- backend/ocr-service/src/main/java/com/posco/ocrservice/OcrServiceApplication.java
+++ backend/ocr-service/src/main/java/com/posco/ocrservice/OcrServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.ocr_service;
+package com.posco.ocrservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
