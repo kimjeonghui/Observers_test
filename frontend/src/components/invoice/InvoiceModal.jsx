a6814392c58e904663129f1265037ff737f8f894
SuGyoungIn <isks2838@gmail.com>
2024-02-15 09:26:20 +0900

    [FE] feat: 거래내역 입력창 input 추가(상태관리는 되어있지 않음)

--- frontend/src/components/invoice/InvoiceModal.jsx
+++ frontend/src/components/invoice/InvoiceModal.jsx
@@ -8,6 +8,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  TextField,
 } from '@mui/material';
 import { ModalStrokeBtn } from './InvoiceStyles';
 import CustomButton from '../global/Button';
@@ -17,7 +18,12 @@ function InvoiceModal(props) {
   const [steps, setSteps] = useState(1);
   const [dep_curr, setDep_curr] = useState();
   const [wd_curr, setWd_curr] = useState();
-  const handleClose = () => setOpen(false);
+  //Todo: 거래내역 입력창들에 대한 변동값 만들어줘야함
+
+  const handleClose = () => {
+    setSteps(1);
+    setOpen(false);
+  };
 
   const handleSteps = (step) => {
     setSteps(() => step + 1);
@@ -98,10 +104,63 @@ function InvoiceModal(props) {
           <div>
             <DialogContent>
               <Input
-                widthV='10'
+                widthV='30'
                 heightV='5'
                 label='거래처명'
                 placeholder='거래처명을 입력하세요'
+              />
+              <Input
+                widthV='10'
+                heightV='5'
+                label='식별코드'
+                placeholder='거래처명을 입력하세요'
+              />
+              <br />
+              <Select
+                labelId='dep_curr'
+                id='demo-simple-select'
+                value={dep_curr}
+                label='입금통화'
+                onChange={handleDepChange}
+              >
+                <MenuItem value='ARS'>ARS</MenuItem>
+                <MenuItem value='USD'>USD</MenuItem>
+                <MenuItem value='KRW'>KRW</MenuItem>
+              </Select>
+              <Input
+                widthV='10'
+                heightV='5'
+                label='입금금액'
+                placeholder='입금금액을 입력하세요'
+              />
+              <Select
+                labelId='dep_curr'
+                id='demo-simple-select'
+                value={wd_curr}
+                label='출금통화'
+                onChange={handleWdChange}
+              >
+                <MenuItem value='ARS'>ARS</MenuItem>
+                <MenuItem value='USD'>USD</MenuItem>
+                <MenuItem value='KRW'>KRW</MenuItem>
+              </Select>
+              <Input
+                widthV='10'
+                heightV='5'
+                label='출금금액'
+                placeholder='출금금액을 입력하세요'
+              />
+
+              <Input widthV='10' heightV='5' label='거래일자' type='date' />
+              <br />
+              <TextField
+                sx={{ width: '30vw' }}
+                id='standard-multiline-static'
+                label='Multiline'
+                multiline
+                rows={4}
+                defaultValue='Default Value'
+                variant='standard'
               />
             </DialogContent>
           </div>
@@ -112,7 +171,7 @@ function InvoiceModal(props) {
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
+    <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth='md'>
       {renderDialogContent()}
     </Dialog>
   );
