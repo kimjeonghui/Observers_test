b5dd5469292342a0cb5e15220e10c41e849a8fc9
SuGyoungIn <isks2838@gmail.com>
2024-02-16 17:54:51 +0900

    [FE] feat: 증빙자료별로 디테일페이지로 이동하는 버튼(기능은 안되있음) 추가

--- frontend/src/components/invoice/InvoiceEviModal.jsx
+++ frontend/src/components/invoice/InvoiceEviModal.jsx
@@ -11,13 +11,18 @@ export default function InvoiceEviModal(props) {
     <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth='lg'>
       <DialogTitle>증빙자료 조회</DialogTitle>
       <DialogContent>
+        <p>증빙자료는 최대 3개까지 가능합니다.</p>
         {datas?.map((data) => {
           return (
-            <img
-              key={data.eviId}
-              alt={'증빙자료' + data.eviId}
-              src={data.img}
-            />
+            <div>
+              <img
+                key={data.eviId}
+                alt={'증빙자료' + data.eviId}
+                src={data.img}
+                style={{ width: '30%' }}
+              />
+              <CustomButton>디테일 페이지 이동</CustomButton>
+            </div>
           );
         })}
         {datas.length < 3 && <CustomButton>증빙자료 추가</CustomButton>}
