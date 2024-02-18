d7f31d2012c44e91baf84e3aec8c24bb7880910d
kimjoenghui <20190896@sungshin.ac.kr>
2024-02-18 14:35:19 +0900

    [BE] feat: 공통 엔티티 추가

---
+++ backend/accounting-service/src/main/java/com/posco/accountingservice/entity/AccountingSlipInvoiceNumEntity.java
@@ -0,0 +1,31 @@
+package com.posco.accountingservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "pos_ovs_accounting_slip_invoice_num")
+public class AccountingSlipInvoiceNumEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long invoiceNum;
+
+    @Column(name = "invoice_data_id", insertable = false, updatable = false)
+    private Long invoiceDataId;
+
+    @OneToOne(targetEntity = InvoiceDataEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "invoice_data_id")
+    private InvoiceDataEntity invoiceDataEntity;
+
+    @OneToMany(mappedBy = "accountingSlipInvoiceNumEntity", cascade = CascadeType.REMOVE)
+    private List<AccountingSlipEntity> accountingSlipEntityList;
+}
---
+++ backend/accounting-service/src/main/java/com/posco/accountingservice/entity/InvoiceDataEntity.java
@@ -0,0 +1,65 @@
+package com.posco.accountingservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "pos_ovs_invoice_data")   // 거래 자료
+public class InvoiceDataEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long invoiceDataId;
+
+    @Column
+    private LocalDateTime txDate;       //
+
+    @Column
+    private String store;               // 거래처명
+
+    @Column
+    private String depCurr;             // 입금 통화
+
+    @Column
+    private BigDecimal deposit;         // 입금 금액
+
+    @Column
+    private String wdCurr;              // 출금 통화
+
+    @Column
+    private BigDecimal withdrawal;      // 출금 금액
+
+    @Column(columnDefinition = "TEXT")
+    private String description;         // 거래 내역
+
+    @Column
+    private BigDecimal transAmount;     // 환산 금액
+
+    @Column
+    private String tranCd;              // 식별 코드
+
+    @Column
+    private String ovsCd;               // 사무소 코드
+
+    @Column
+    private LocalDateTime fiscalMonth;  // 회계 년월
+
+    @Column
+    private String status;              // 상태
+
+    @Column
+    private Long ocrId;                 // OCR ID
+
+    @OneToMany(mappedBy = "invoiceDataEntity", cascade = CascadeType.REMOVE)
+    private List<EvidenceDataEntity> evidenceDataEntityList;
+}
