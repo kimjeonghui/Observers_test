be47f2fd567ed4e43a2026da234c3a9c500bacac
daeil <rkim98813@gmail.com>
2024-02-22 15:18:50 +0900

    [BE] Fix: glcode service DTO Entity 컬럼 수정 및 추가

--- backend/glcode-service/src/main/java/com/posco/glcodeservice/entity/GLCodeEntity.java
+++ backend/glcode-service/src/main/java/com/posco/glcodeservice/entity/GLCodeEntity.java
@@ -13,47 +13,53 @@ import javax.persistence.*;
 @Getter
 @Entity
 @Table(name = "pos_ovs_gl_code")
-public class GLCodeEntity{
+public class GLCodeEntity extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long glCodeId;                             // AUTO_INCREMENT ID
-
-    @Column
-    private String tranCd;
-
-    @Column
-    private String accountName;
-
-    @Column
-    private Long account;
-
-    @Column
-    private Long subAccount;
-
-    @Column
-    private Long depositCd;
-
-    @Column
-    private String deptReqFlag;
-
-    @Column
-    private String majorCt;
-
-    @Column
-    private String mediumCt;
-
-    @Column
-    private String minorCt;
+    private Long glCodeId;                                          // AUTO_INCREMENT ID
 
     @Column(name = "ovs_cd", insertable = false, updatable = false)
     private String ovsCd;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ovs_cd", referencedColumnName = "ovs_cd")
+    private ReferenceEntity referenceEntity;
+
+    @Column
+    private String tranCd;                                          // 식별코드
+
+    @Column
+    private String accountName;                                     // 계정명
+
+    @Column
+    private Long account;                                           // 계정코드
+
+    @Column
+    private Long subAccount;                                        // 보조계정
+
+    @Column
+    private Long depositCd;                                         // 입출금구분
+
+    @Column
+    private String deptReqFlag;                                     // 부서코드 필수여부
+
+    @Column
+    private String majorCt;                                         // 대분류
+
+    @Column
+    private String mediumCt;                                        // 중분류
+
+    @Column
+    private String minorCt;                                         // 소분류
+
+    @Column
+    private String description;                                     // 적요설명
+
+    @Column
+    private String additionalComment;                               // 비고
+
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "ovs_cd", referencedColumnName = "ovs_cd")
 //    private ReferenceEntity referenceEntity;
-
-
-
-
 
 }
