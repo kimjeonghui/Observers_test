f69d0c026167f2fb5c11fc271452ae811acc017c
daeil <rkim98813@gmail.com>
2024-02-23 01:16:38 +0900

    [BE] Feat: 환율 서비스 Repository FROM 통화, TO 통화 List로 가져오기

---
+++ backend/exchange-service/src/main/java/com/posco/exchangeservice/repository/ExchangeRateRepository.java
@@ -0,0 +1,13 @@
+package com.posco.exchangeservice.repository;
+
+import com.posco.exchangeservice.entity.ExchangeRateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRateEntity, String>, ExchangeRateRepositoryCustom {
+
+    List<ExchangeRateEntity> findByFromCurr(String fromCurr);
+    List<ExchangeRateEntity> findByToCurr(String toCurr);
+
+}
