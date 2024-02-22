3a17e866f55f97e05e68297198e6eeab82324901
daeil <rkim98813@gmail.com>
2024-02-22 15:23:02 +0900

    [BE] Feat: glcode service ReferenceEntity 엔티티 생성

---
+++ backend/glcode-service/src/main/java/com/posco/glcodeservice/entity/ReferenceEntity.java
@@ -0,0 +1,55 @@
+package com.posco.glcodeservice.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "pos_ovs_cost_center")
+public class ReferenceEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "ovs_cd", nullable = false)
+    private String ovsCd;           // 사무소 코드
+
+    @Column(nullable = false)
+    private String ovsMeaning;      // 사무소 코드 의미
+
+    @Column(nullable = false)
+    private String ovsCopCd;        // 법인 코드
+
+    @Column(nullable = false)
+    private String glCurr;          // 장부 통화
+
+    @Column(nullable = false)
+    private String locCurr;         // 현지 통화1
+
+    @Column
+    private String locCurr2;        // 현지 통화2
+
+    @Column(nullable = false)
+    private String transCurr;       // 송금 통화1
+
+    @Column
+    private String transCurr2;      // 송금 통화2
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;      // 만료일
+
+    @OneToMany(mappedBy = "referenceEntity", cascade = CascadeType.REMOVE)
+    private List<GLCodeEntity> glCodeEntityList;
+
+//    @OneToMany(mappedBy = "referenceEntity", cascade = CascadeType.REMOVE)
+//    private List<PeriodStatusEntity> periodStatusEntityList;
+
+}
