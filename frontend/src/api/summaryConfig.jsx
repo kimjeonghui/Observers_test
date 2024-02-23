abe8dec02733f4d911ee72eaa1d608e7c4b58142
csh <tmdgml1754@naver.com>
2024-02-23 17:56:12 +0900

    [FE] Feat: 월 총괄표 페이지 표

---
+++ frontend/src/api/summaryConfig.jsx
@@ -0,0 +1,15 @@
+//변수명은 notion의 API명세서에 method+기능을 참고하였음.
+
+const requests = {
+  //기본 URL 주소
+  base_url: process.env.REACT_APP_SUMMARY_API,
+
+  POST_SUMMARY() {
+    return this.base_url + `/summary`;
+  },
+  GET_SUMMARY(ovsCd, fiscalMonth) {
+    return this.base_url + `/summary/${ovsCd}/${fiscalMonth}`;
+  },
+};
+
+export default requests;
