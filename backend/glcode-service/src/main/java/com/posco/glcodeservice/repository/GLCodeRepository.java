359683d48fb83c2eaef06ce8b4b9b072c89c5175
daeil <rkim98813@gmail.com>
2024-02-23 16:42:16 +0900

    [BE] Feat: 식별코드관리 Repository 식별코드, 대분류, glCodeId로 찾기 생성

--- backend/glcode-service/src/main/java/com/posco/glcodeservice/repository/GLCodeRepository.java
+++ backend/glcode-service/src/main/java/com/posco/glcodeservice/repository/GLCodeRepository.java
@@ -3,10 +3,14 @@ package com.posco.glcodeservice.repository;
 import com.posco.glcodeservice.entity.GLCodeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GLCodeRepository extends JpaRepository<GLCodeEntity, String>, GLCodeRepositoryCustom {
 
-    GLCodeEntity findByTransCd(String transCd);
+    GLCodeEntity findByTranCd(String tranCd);
 
-    GLCodeEntity findByMajorCt(String majorCt);
+    List<GLCodeEntity> findByMajorCt(String majorCt);
+
+    GLCodeEntity findByGlCodeId(Long glCodeId);
 
 }
